a04e5aa55c2880a744eebcf53cc3f44934ad6c7e
beautiful-pixel <duchiron.4@gmail.com>
2025-12-13 07:42:05 +0100

    ajout du module data - à compléter

---
+++ src/data/__init__.py
@@ -0,0 +1,24 @@
+# Categories
+from .categories import (
+    CATEGORY_NAMES,
+    CATEGORY_SHORT_NAMES,
+    CATEGORY_GROUPS,
+    get_category_name,
+    get_all_categories,
+    get_category_codes,
+    get_category_group,
+    format_category_label,
+    map_categories_in_dataframe,
+    get_category_distribution,
+    validate_category_code,
+    print_category_summary,
+)
+
+# Récupération des données
+from .load_data import(
+    split_data,
+    split_path,
+    split_txt,
+    get_mask,
+    images_read,
+)
