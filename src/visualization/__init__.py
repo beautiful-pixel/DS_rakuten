a43a8b4e863a38d19ee787527233a7eb94810772
beautiful-pixel <duchiron.4@gmail.com>
2026-01-07 00:44:36 +0100

    plot_report & ajout du modèle baseline image

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -1,8 +1,9 @@
 from .image_grids import images_grid
 from .features_importance import plot_features_importance
+from .report import plot_classification_report
 
 __all__ = [
     "images_grid",
     "plot_features_importance",
-
+    "plot_classification_report"
 ]
